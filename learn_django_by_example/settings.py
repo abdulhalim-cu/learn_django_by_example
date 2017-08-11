dfe0e5f94c0f837aa969e734c66d817bfa270f38
abdulhalim-cu <abdulhalim.cu10@gmail.com>
2017-08-11 21:59:09 +0600

    database created

--- learn_django_by_example/settings.py
+++ learn_django_by_example/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'motorcontroller.apps.MotorcontrollerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (os.path.join(BASE_DIR,  'static_root'))
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
